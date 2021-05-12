f7dfe2f34f65409c6a91c8fdc02f7f100e949ae6
ypisarev <pisarev.pl@gmail.com>
2021-05-12 21:40:04 +0300

    Adder base operation

---
+++ l04/variables.php
@@ -0,0 +1,18 @@
+<?php
+
+$fruit = 'apple';
+$fruit = 'banana';
+
+echo $fruit, '<br>';
+
+//$1test = 1;
+$te2s5t = 1;
+echo $te2s5t, '<br>';
+
+//$еуіе = 123ж
+
+$my_she_lk = 'Olya Pisareva';
+$mySheLk = 'Pisareva Olya';
+
+echo $my_she_lk, '<br>',  $mySheLk;
+
---
+++ l05/constants.php
@@ -0,0 +1,14 @@
+<?php
+
+define('APP_VERSION', 0.05);
+//define('APP_VERSION', 0.06);
+//APP_VERSION = 22;
+
+echo APP_VERSION, '<br>';
+
+const APP_NAME = 'Lesson5';
+//const APP_NAME = 'Lesson5';
+//define('APP_NAME', 0.06)
+echo APP_NAME;
+
+var_dump(__FILE__, __LINE__);
